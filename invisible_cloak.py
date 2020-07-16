616400a5cc96ea63cda241fca857d2400aee563b
Harshit Singh <harshit9665@gmail.com>
2020-07-16 11:13:56 +0530

    Add files via upload

---
+++ invisible_cloak.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+back = cv2.imread('./image.jpg')
+
+while cap.isOpened():
+    # take each frame
+    ret, frame = cap.read()
+    if ret:
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        # lower: hue - 10, 100, 100, higher: h+10, 255, 255
+        red = np.uint8([[[0,0,255]]]) # bgr value of red
+        hsv_red = cv2.cvtColor(red, cv2.COLOR_BGR2HSV)
+        # get hsv value of red from bgr
+        l_red = np.array([0, 100, 100])
+        u_red = np.array([10, 255, 255])
+
+        mask = cv2.inRange(hsv, l_red, u_red)
+
+        # all things red
+        part1 = cv2.bitwise_and(back, back, mask=mask)
+        mask = cv2.bitwise_not(mask)
+
+        # part 2 is all things not red
+        part2 = cv2.bitwise_and(frame, frame, mask=mask)
+        cv2.imshow("cloak", part1 + part2)
+        if cv2.waitKey(5) == ord('q'):
+            break
+cap.release()
+cv2.destroyAllWindows()
